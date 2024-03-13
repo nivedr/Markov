4adcb10424f7016b3528e7fdac8c21f37c077461
Nived Rajaraman <14037292+nivedr@users.noreply.github.com>
2024-03-13 16:57:17 -0400

    fix model

--- Markov-LLM-k/src/tokenizer/train_tokenizer.py
+++ Markov-LLM-k/src/tokenizer/train_tokenizer.py
@@ -15,16 +15,16 @@ import pickle
 import yaml
 from pathlib import Path
 
-# Instantiate tokenizer model
+# Instantiate tokenizer
 def train_tokenizer(tokenizer, max_dict_size, p, q, order, generator, dataset_size, extra_args):
     print(f'Training tokenizer: {tokenizer}')
     dataset, _ = utils.get_batch(p, q, order, seq_length=1, batch_size=dataset_size, generator=generator, extra_args=extra_args, device=extra_args.device)
-    if model == 'Character':
+    if tokenizer == 'Character':
         tokenizer = Tokenizer.Tokenizer()
         tokenizer.learn_dict()
-    elif model == 'BPE':
+    elif tokenizer == 'BPE':
         tokenizer = BPE.BPE(kappa=kappa, max_dict_size=max_dict_size, dataset=dataset)
         tokenizer.learn_dict()
     else:
-        raise ValueError('model must be either Character, LZW, BPE or SplitBPE')
+        raise ValueError('Tokenizer must be either Character, LZW, BPE or SplitBPE')
     return tokenizer
