e5870d388e1986130cfac60ed595e8146d83edac
Nived Rajaraman <14037292+nivedr@users.noreply.github.com>
2024-03-13 17:59:58 -0400

    remove .to('cuda')

--- Markov-LLM-k/src/optim/base.py
+++ Markov-LLM-k/src/optim/base.py
@@ -32,7 +32,7 @@ def train_base(model, tokenizer, opt, p, q, order, scheduler, iterations, acc_st
     while itr < iterations:
         for microstep_idx in range(acc_steps):  # gradient accumulation
             x, y = get_batch(p, q, order, sequence_length, batch_size, generator, extra_args, device=extra_args.device)
-            x = tokenizer.encode_batch(x).to('cuda')
+            x = tokenizer.encode_batch(x)
             y = x[:,1:]
             with type_ctx:
                 with distributed_backend.get_context_for_microstep_forward(model=model, microstep_idx=microstep_idx, gradient_accumulation_steps=acc_steps):
