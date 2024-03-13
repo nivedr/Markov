2bf2f9839d11709d511ea5f1e4040c5ec9431ab1
Nived Rajaraman <14037292+nivedr@users.noreply.github.com>
2024-03-13 18:14:37 -0400

    deepcopy

--- Markov-LLM-k/src/optim/base.py
+++ Markov-LLM-k/src/optim/base.py
@@ -9,6 +9,8 @@ import sys
 sys.path.append('../tokenizer')
 import Tokenizer
 import BPE
+from copy import deepcopy
+
 
 from .utils import eval, eval_probs, get_batch, save_checkpoint
 
@@ -33,7 +35,7 @@ def train_base(model, tokenizer, opt, p, q, order, scheduler, iterations, acc_st
         for microstep_idx in range(acc_steps):  # gradient accumulation
             x, y = get_batch(p, q, order, sequence_length, batch_size, generator, extra_args, device=extra_args.device)
             x = tokenizer.encode_batch(x)
-            y = x[:,1:].to("cuda")
+            y = deepcopy(x[:,1:]).to("cuda")
             x = x[:,:-1].to("cuda")
             with type_ctx:
                 with distributed_backend.get_context_for_microstep_forward(model=model, microstep_idx=microstep_idx, gradient_accumulation_steps=acc_steps):
