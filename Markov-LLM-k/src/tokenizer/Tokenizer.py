23c4284d69870e942336474b72666d9b2948b981
Nived Rajaraman <14037292+nivedr@users.noreply.github.com>
2024-03-16 14:42:09 -0400

    encode_batch

--- Markov-LLM-k/src/tokenizer/Tokenizer.py
+++ Markov-LLM-k/src/tokenizer/Tokenizer.py
@@ -5,31 +5,33 @@ import math
 
 
 class Tokenizer(object):
+    _dset_tok = []
 
-	_dset_tok = []
-
-	DS = []
-	max_dict_size = -1
-	dict_size = 0
-	dictionary = []
+    DS = []
+    max_dict_size = -1
+    dict_size = 0
+    dictionary = []
 
 
-	def __init__(self, max_dict_size=-1):
-		self.max_dict_size = max_dict_size
-	
-	def learn_dict(self):
-		self.dictionary = range(2)
+    def __init__(self, max_dict_size=-1):
+        self.max_dict_size = max_dict_size
 
-	def encode(self, string):
-		"Encode string"
-		return string
+    def learn_dict(self):
+        self.dictionary = range(2)
 
-	def decode(self, string):
-		"Decode string"
-		return string
+    def encode(self, string):
+        "Encode string"
+        return string
 
-	def count(self, string, tok):
-		return collections.Counter(string)[tok]
+    def decode(self, string):
+        "Decode string"
+        return string
 
-	def all_counts(self, string):
-		return collections.Counter(string)
+    def count(self, string, tok):
+        return collections.Counter(string)[tok]
+
+    def encode_batch(self, batch):
+        return batch
+
+    def all_counts(self, string):
+        return collections.Counter(string)
