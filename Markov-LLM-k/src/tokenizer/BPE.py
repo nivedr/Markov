b7bf7d128fd3af43e4ff60f0affd0e8099f45182
Nived Rajaraman <14037292+nivedr@users.noreply.github.com>
2024-03-28 16:05:57 -0400

    msk

--- Markov-LLM-k/src/tokenizer/BPE.py
+++ Markov-LLM-k/src/tokenizer/BPE.py
@@ -21,10 +21,11 @@ class BPE(Tokenizer.Tokenizer):
         t1,t2 = self.DS[tok-2]
         print(f't1 = {t1}')
         print(f't2 = {t2}')
+        mask = string[:-1]
         if t1 != t2:
             mask = (string[:-1] == t1) and (string[1:] == t2)
         else:
-            for j in range(len(string)):
+            for j in range(len(string)-1):
                 mask[j] = (string[j] == string[j+1]) and (j==0 or mask[j-1] != True)
 
         string[:-1][mask] = tok
