797d5497c80aaeacc794194800ccb3616757a212
Nived Rajaraman <14037292+nivedr@users.noreply.github.com>
2024-03-28 16:24:09 -0400

    src

--- Markov-LLM-k/src/tokenizer/BPE.py
+++ Markov-LLM-k/src/tokenizer/BPE.py
@@ -10,7 +10,7 @@ from collections import deque
 class BPE(Tokenizer.Tokenizer):
 
     kappa = 10
-	
+
 
     def __init__(self, kappa, max_dict_size):
         self.kappa = kappa
@@ -19,30 +19,24 @@ class BPE(Tokenizer.Tokenizer):
 
     def apply(self, string, tok):
         t1,t2 = self.DS[tok-2]
-        print(f't1 = {t1}')
-        print(f't2 = {t2}')
-        mask = string[:-1].copy()
 
-        if t1 != t2:
-            mask = (string[:-1] == t1) and (string[1:] == t2)
-        else:
-            for j in range(len(string)-1):
-                mask[j] = (string[j] == string[j+1]) and (j==0 or mask[j-1] != True)
-
-        string[:-1][mask] = tok
-    
-        mask0 = deque(mask)
-        mask0.appendleft(0)
-        mask = list(mask0)
-
-        return string[(mask[:-1] == False)]
-
+        i=0
+        string_new = []
+        while i < len(string):
+            if i < len(string)-1 and string[i] == t1 and string[i+1] == t2:
+                string_new.append(tok)
+                i+=2
+            else:
+                string_new.append(string[i])
+                i+=1
+        self._dset_tok = string_new
+        return string_new
 
     def unapply(self, string, tok):
         t1,t2 = self.DS[tok-2]
 
         i=0
-        string_new = torch.empty(0)
+        string_new = []
         while i < len(string):
             if string[i] == tok:
                 string_new.append(t1)
@@ -50,8 +44,8 @@ class BPE(Tokenizer.Tokenizer):
             else:
                 string_new.append(string[i])
             i+=1
-        self._dset_tok = torch.tensor(string_new)
-        return string_new
+        self._dset_tok = string_new
+        return string
 
 
     def pairwise_count(self, string, t1, t2):
@@ -64,11 +58,12 @@ class BPE(Tokenizer.Tokenizer):
                 i += 1
         return val
 
+
     def all_pairwise_counts(self, string):
         count_mat = np.zeros([self.dict_size,self.dict_size])
         repeat = False
-
-        i=0
+        
+		i=0
         while i < len(string)-1:
             t1 = string[i]
             t2 = string[i+1]
@@ -81,28 +76,29 @@ class BPE(Tokenizer.Tokenizer):
             else:
                 repeat = False
                 count_mat[t1,t2] += 1
-             
+
             i += 1
 
         return count_mat
+
 
     def add_token(self, dset):
         apc = self.all_pairwise_counts(dset)
         max_bigram = np.max(apc)
         argmax_bigram = list(np.unravel_index(np.argmax(apc, axis=None), apc.shape))
-	
+
         if max_bigram >= self.kappa:
-            self.DS.append(argmax_bigram)
-            # return True if a token was added
-            return True
+                self.DS.append(argmax_bigram)
+                # return True if a token was added
+                return True
         return False
 
     def learn_dict(self, dset=None):
         print("Learning dictionary using BPE...")
         if dset is not None:
-            self._dset_tok = np.array(dset.tolist())
+            self._dset_tok = dset
         else:
-            return print("No dictionary was learned since dataset was None")
+            raise ValueError('No Dict')
         self.DS = []
 
         while len(self._dset_tok) > 1:
@@ -110,7 +106,7 @@ class BPE(Tokenizer.Tokenizer):
             self.dictionary = range(self.dict_size)
             if self.dict_size < self.max_dict_size and self.add_token(self._dset_tok):
                 new_tok = len(self.DS)+1
-                self._dset_tok = self.apply(self._dset_tok, new_tok)
+                self.apply(self._dset_tok, new_tok)
             else:
                 break
 
@@ -118,12 +114,12 @@ class BPE(Tokenizer.Tokenizer):
 
 
     def encode(self, string):
-        assert set(string).issubset(set(range(2))), "String contains elements outside alphabet"
-        self._dset_tok = np.array(string)
-        
+        self._dset_tok = string
+        assert set(self._dset_tok).issubset(set(range(2))), "String contains elements outside alphabet"
+
         for tok_i in range(len(self.DS)):
-            self._dset_tok = self.apply(self._dset_tok, tok_i+2)
-        return torch.tensor(self._dset_tok)
+            self.apply(self._dset_tok, tok_i+2)
+        return self._dset_tok
 
 
     def decode(self, string):
