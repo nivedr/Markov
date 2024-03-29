2f5e728b97c13052563df88615843349eabafff0
Nived Rajaraman <14037292+nivedr@users.noreply.github.com>
2024-03-29 15:30:50 -0400

    lzw

--- Markov-LLM-k/src/main.py
+++ Markov-LLM-k/src/main.py
@@ -25,8 +25,8 @@ import distributed
 def get_args():
     parser = argparse.ArgumentParser(allow_abbrev=False)
     parser.add_argument('--config_format', default='markov', choices=config.registered_formats())
-    parser.add_argument('--tokenizer', default='BPE', choices=config.registered_formats())
-    parser.add_argument('--max_dict_size', default=20, choices=config.registered_formats())
+    parser.add_argument('--tokenizer', default='LZW', choices=config.registered_formats())
+    parser.add_argument('--max_dict_size', default=10, choices=config.registered_formats())
     parser.add_argument('--dataset_size', default=10000, choices=config.registered_formats())
 
     args, rem_args = parser.parse_known_args()
