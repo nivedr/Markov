d6bbf9e06b16383a717f2c3c9d17091dfce1b0bf
Nived Rajaraman <14037292+nivedr@users.noreply.github.com>
2024-04-26 03:51:38 -0700

    cpu

--- Markov-LLM-k/src/optim/utils.py
+++ Markov-LLM-k/src/optim/utils.py
@@ -29,7 +29,7 @@ def get_batch(P, order, seq_length, batch_size, generator, extra_args, device='c
 
 def get_next_symbols(P, data, switching=True):
     order = data.size(dim=1)
-    bool_to_int = torch.tensor([2**i for i in range(order)]).to('cuda')
+    bool_to_int = torch.tensor([2**i for i in range(order)]).to('cpu')
     idx = torch.sum(torch.mul(data, bool_to_int[None,:]), dim=1)
     
     M = P[idx]
