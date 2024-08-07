a755702427be56cd0541c56ff70d37ae3ce60db8
nivedr <nived.rajaraman@gmail.com>
2024-08-07 01:47:57 +0000

    add stuff

--- Markov-LLM-k/src/run.sh
+++ Markov-LLM-k/src/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
-python main.py --p 0.8 --q 0.8 --n_layer 2 --n_head 1 --n_embd 50 --sequence_length 200 --order 1 --data_in_ram --iterations 20000 --batch_size 8 --acc_steps 1 --transition random
+python main.py --p 0.8 --q 0.8 --n_layer 2 --n_head 2 --n_embd 50 --sequence_length 200 --order 2 --vocab_size 2 --data_in_ram --iterations 500 --batch_size 8 --acc_steps 1 --transition  random
 mv /data/nived/Markov/Markov-LLM-k/src/val-loss-dump.pickle /data/nived/Markov/Markov-LLM-k/src/interpolation_2L_2H/interp_100_iter_$n.rm -r /data/nived/Markov/Markov-LLM-k/src/exps/markov/base
 
